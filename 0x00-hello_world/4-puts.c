aa4d79d7da331d1344745ac98bb09e79953bbba6
fatma <fatmacivil@hotmail.com>
2023-09-06 23:29:05 -0700

    666

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: A C program that prints with puts function
+ *
+ * Return: Always 0 (success)
+*/
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
+
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 
 }
