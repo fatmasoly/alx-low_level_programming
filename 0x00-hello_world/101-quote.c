15ee8f03e0d3bacbbe63d069776dba685037c9a6
fatma <fatmacivil@hotmail.com>
2023-09-07 06:28:40 -0700

    roo

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,10 +8,12 @@
  * Return: Always 1 (not success)
 */
 
-int main(void){
+int main(void)
 
-const char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
-; write(2, quote, 59);
+{
+
+char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+, write(2, quote, 59);
 return (1);
 
 }
