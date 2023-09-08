47d9a3fd1f439f10f6aac6fbb8e98e89b00c1b80
fatma <fatmacivil@hotmail.com>
2023-09-08 16:23:33 -0700

    final

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,7 +11,7 @@ int main(void)
 int n1, n2;
 for (n1 = 0 ; n1 <= 98 ; n1++)
 {
-for (n2 = 0 ; n2 <= 99 ; n2++)
+for (n2 = n1 + 1 ; n2 <= 99 ; n2++)
 {
 putchar('0' + (n1 / 10));
 putchar('0' + (n1 % 10));
@@ -20,15 +20,17 @@ putchar('0' + (n2 / 10));
 putchar('0' + (n2 % 10));
 if (n1 != 98 || n2 != 99)
 {
-putchar(' ');
 putchar(',');
+putchar(' ');
 
 }
 
 }
 
 }
+
 putchar('\n');
 return (0);
 
 }
+
