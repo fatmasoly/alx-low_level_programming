d50d3b5590c02bc60f10de140c5a02cf5fb89139
fatma <fatmacivil@hotmail.com>
2023-09-08 03:35:12 -0700

    eee

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Point
+ *
+ * Description: A C program that prints with puts function
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+char ch1 = 'a';
+char ch2 = 'A';
+while (ch1 <= 'z')
+{
+putchar(ch1);
+ch1++;
+}
+while (ch2 <= 'Z')
+{
+putchar(ch2);
+ch2++;
+}
+putchar('\n');
+return (0);
+
+}
