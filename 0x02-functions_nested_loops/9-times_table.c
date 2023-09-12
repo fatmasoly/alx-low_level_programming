bb2f4e83b73d975c2517c773c00fef2a4ceb6c79
fatma <fatmacivil@hotmail.com>
2023-09-12 08:09:35 -0700

    hh

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,29 +6,29 @@
 void times_table(void)
 {
 int raw, column, x;
-for (raw = 0 ; raw < 9 ; raw++)
+for (raw = 0 ; raw <= 9 ; raw++)
 {
-for (column = 0 ; column < 9 ; column++)
+for (column = 0 ; column <= 9 ; column++)
 {
 x = raw * column;
 if (column == 0)
 {
-_putchar('0');
+_putchar(x + '0');
 }
-else
+if (x < 10 && column != 0)
 {
 _putchar(',');
 _putchar(' ');
-}
-if (x < 10)
-{
 _putchar(' ');
+_putchar(x + '0');
 }
-else
+else if (x >= 10)
 {
-_putchar(x / 10 + '0');
+_putchar(',');
+_putchar(' ');
+_putchar((x / 10) + '0');
+_putchar((x % 10) + '0');
 }
-_putchar(x % 10 + '0');
 }
 _putchar('\n');
 }
