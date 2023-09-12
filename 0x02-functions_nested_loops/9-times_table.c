3e3d0cfb9957a2d915b668c4af8a800699b286b7
fatma <fatmacivil@hotmail.com>
2023-09-12 07:47:32 -0700

    rw

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,9 +6,9 @@
 void times_table(void)
 {
 int raw, column, x;
-for (raw = 0 ; raw <= 9 ; raw++)
+for (raw = 0 ; raw < 10 ; raw++)
 {
-for (column = 0 ; column <= 9 ; column++)
+for (column = 0 ; column < 10 ; column++)
 {
 x = raw * column;
 if (column == 0)
