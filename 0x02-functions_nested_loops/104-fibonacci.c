d5d56ed4d25d35cfd1a43cc566e81ea8fea1cbdd
fatma <fatmacivil@hotmail.com>
2023-09-12 13:52:22 -0700

    final

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - fibonacci <3
+ *
+ * Purpose - no hardcode
+ *
+ * Return:  (Success)
+ */
+int main(void)
+{
+int i;
+unsigned int first = 1, second = 2, next;
+for (int i = 2; i < 98; i++)
+{
+next = first + second;
+if (i < 97)
+{
+printf("%u, ", next);
+}
+else
+{
+printf("%u\n", next);
+}
+first = second;
+second = next;
+}
+return (0);
+}
+
