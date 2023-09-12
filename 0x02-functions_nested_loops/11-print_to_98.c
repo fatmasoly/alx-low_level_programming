f386399e0cff9802fcd3ec42e322cd76f7aad3bd
fatma <fatmacivil@hotmail.com>
2023-09-12 10:50:20 -0700

    0707

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,27 +7,26 @@
  */
 void print_to_98(int n)
 {
+int i, j;
 if (n <= 98)
-for (int i = 0 ; i <= 98 ; i++)
 {
+for (i = n; i <= 98; i++)
+{
+if (i != 98)
 printf("%d, ", i);
-if (i < 98)
+else if (i == 98)
+printf("%d\n", i);
+}
+}
+else if (n >= 98)
 {
-printf(", ");
-}
-}
-else
+for (j = n; j >= 98; j--)
 {
-for (int i = n ; i >= 98 ; i--)
-{
-printf("%d, ", i);
-if (i > 98)
-{
-printf(", ");
+if (j != 98)
+printf("%d, ", j);
+else if (j == 98)
+printf("%d\n", j);
 }
 }
-}
-printf("\n");
-return (0);
 }
 
