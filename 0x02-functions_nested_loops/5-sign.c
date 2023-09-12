30d92c0a08334ae298dabd52cd0e0bfe4193f72e
fatma <fatmacivil@hotmail.com>
2023-09-12 06:02:51 -0700

    uu

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * main - Point
- *
- * Description: A C program that prints with puts function
- *
- * Return: Always 0 (success)
-*/
-int print_sign(int n);
+ * print_sign - prints the sign of a number
+ * @n: the int to check
+ * Return: 1 and prints + if n is greater than zero
+ * 0 and prints 0 if n is zero
+ * -1 and prints - if n is less than zero
+ */
+int print_sign(int n)
 {
 if (n > 0)
 {
