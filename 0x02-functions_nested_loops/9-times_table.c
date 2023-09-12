f728068c258e1ad0b6c82684c435c6d1275bb744
fatma <fatmacivil@hotmail.com>
2023-09-12 07:39:28 -0700

    raw

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0
+ */
+void times_table(void)
+{
+int raw, column, x;
+for (raw = 0 ; raw <= 9 ; raw++)
+{
+for (column = 0 ; column <= 9 ; column++)
+{
+x = raw * column;
+if (column == 0)
+{
+_putchar(x + '0');
+}
+else
+{
+_putchar(',');
+_putchar(' ');
+if (x < 10)
+{
+_putchar(' ');
+}
+_putchar(x / 10 + '0');
+_putchar(x % 10 + '0');
+}
+}
+_putchar('\n');
+}
+}
+
