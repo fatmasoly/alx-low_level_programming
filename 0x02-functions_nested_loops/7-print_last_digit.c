72fcdbdbaae4c1c44971ac41f5eefeede1c935ed
fatma <fatmacivil@hotmail.com>
2023-09-12 06:26:18 -0700

    dd

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,18 +1,15 @@
 #include "main.h"
 /**
- * main - Point
- *
- * Description: A C program that prints with puts function
- *
- * Return: Always 0 (success)
-*/
-int print_last_digit(int n);
+ * print_last_digit - prints the last digit of a number
+ * @n: the int to extract the last digit from
+ * Return: value of the last digit
+ */
+int print_last_digit(int n)
 {
 int lastdigit;
 if (lastdigit < 0)
 lastdigit = -1 * (n % 10);
-
-else 
+else
 lastdigit = n % 10;
 _putchar(lastdigit + '0');
 return (lastdigit);
