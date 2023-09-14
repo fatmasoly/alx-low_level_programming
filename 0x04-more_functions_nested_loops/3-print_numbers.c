841e23a6d81e9de0725bdb7e98eeccb46e099a26
fatma <fatmacivil@hotmail.com>
2023-09-13 21:32:41 -0700

    5th

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - print 0-9
+ * return: void
+ */
+void print_numbers(void)
+{
+char ch;
+for (char = '0' ; char <= '9' ; char++)
+{
+_putchar(ch);
+}
+_putchar('\n');
+}
