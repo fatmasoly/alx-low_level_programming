a60b2076e7a7e83f9683c09aab4c1bada33c6e28
fatma <fatmacivil@hotmail.com>
2023-09-14 04:04:38 -0700

    bb

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,3 +7,22 @@
  */
 void print_number(int n)
 {
+unsigned int nu
+if (n < 0)
+{
+nu = -n;
+_putchar('-');
+}
+else
+{
+nu = n;
+}
+
+if (nu / 10)
+{
+print_number(nu / 10);
+}
+
+_putchar((nu % 10) + '0');
+}
+
