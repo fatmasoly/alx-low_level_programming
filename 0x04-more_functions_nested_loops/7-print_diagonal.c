23243aa58049c0b3c29ab1b96b8f40dc100e5243
fatma <fatmacivil@hotmail.com>
2023-09-14 03:11:15 -0700

    yy

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,19 +7,19 @@
 void print_diagonal(int n)
 {
 if (n <= 0)
+{
 _putchar('\n');
+}
+else
+{
 int a, b;
 for (a = 0 ; a < n ; a++)
 {
 for (b = 0 ; b < n ; b++)
 {
 if (a == b)
-{
 _putchar('\\');
-}
-else
-if (a < b)
-{
+else if (a < b)
 _putchar(' ');
 }
 _putchar('\n');
