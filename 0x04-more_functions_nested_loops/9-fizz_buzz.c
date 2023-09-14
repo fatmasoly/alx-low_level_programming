d07a61fcd0d50dec70a40dd7c4681e0ece52d43c
fatma <fatmacivil@hotmail.com>
2023-09-14 03:46:53 -0700

    zz4

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,35 +10,32 @@
 int main(void)
 {
 int i;
-for (i = 1 ; i <= 100 ; i++)
+
+for (i = 1; i <= 100; i++)
 {
-if (i % 3 == 0 && !(i % 5 == 0))
+if (i % 3 == 0 && i % 5 != 0)
 {
-printf("Fizz");
+printf(" Fizz");
 }
-else if
+else if (i % 5 == 0 && i % 3 != 0)
 {
-(i % 5 == 0 && !(i % 3 == 0))
-printf("Buzz");
+printf(" Buzz");
 }
-else if
+else if (i % 3 == 0 && i % 5 == 0)
 {
-(i % 3 == 0 && i % 5 == 0)
-printf("FizzBuzz");
+printf(" FizzBuzz");
 }
-else
+else if (i == 1)
 {
 printf("%d", i);
 }
-if (i != 100)
-{
-printf(" ");
-}
 else
 {
+printf(" %d", i);
+}
+}
 printf("\n");
-}
+
 return (0);
-}
 }
 
