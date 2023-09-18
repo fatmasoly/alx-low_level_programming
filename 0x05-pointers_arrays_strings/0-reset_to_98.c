c5e3521eaff01825cc096194c5a5f38f46889f0a
fatma <fatmacivil@hotmail.com>
2023-09-17 22:28:46 -0700

    000

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * reset_to_98(int *n) a function that takes a pointer to an int as paremeter
+ * reset_to_98 - is a function that takes a pointer to an int as paremeter.
  *
  * @n: int parameter
  *
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * swap_int(int *a, int *b) a function that takes swaps the values of two integers.
+ *
+ * @a & @b: int parameters
+ *
+ * Return nothing
+ */
+void swap_int(int *a, int *b)
+{
+
