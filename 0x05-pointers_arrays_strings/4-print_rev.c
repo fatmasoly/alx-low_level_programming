3e1e933c5bec89bb69b5a08e09de55120fb47830
fatma <fatmacivil@hotmail.com>
2023-09-18 04:22:40 -0700

    400

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,16 +10,24 @@
 void print_rev(char *s)
 {
 char temp;
-int l_index, r_index;
+int l_index = 0;
+int r_index = 0;
+while (s[r_index] != '\0')
+{
+r_index++;
+}
+r_index--;
+
 while (l_index < r_index)
 {
-temp = *s[l_index];
-*s[r_index] = temp;
+temp = s[l_index];
+s[r_index] = temp;
 l_index++;
 r_index--;
+}
+while (*s != '\0')
+{
 _putchar(*s++);
 }
 _putchar('\n');
-
 }
-
