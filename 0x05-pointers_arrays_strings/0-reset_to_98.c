0c2043ce0a4ea32fe6982d70728baf68bc6181ec
fatma <fatmacivil@hotmail.com>
2023-09-17 22:17:03 -0700

    0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * reset_to_98(int *n) a function that takes a pointer to an int as paremeter
+ *
+ * @n: int parameter
+ *
+ * Return nothing
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
+
