7fd8f436620f85af9905ae971758c58e45ca89bc
fatma <fatmacivil@hotmail.com>
2023-09-18 06:00:47 -0700

    jo

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -4,19 +4,18 @@ include "main.h"
  *
  * @str : string parameter
  *
- * Return: (0)
+ * Return: nothing
  *
  */
 void puts2(char *str)
 {
 int i = 0;
-while (*str[i] != '\0')
+while (str[i] != '\0')
 {
-_putchar(*str[i]);
+_putchar(str[i]);
 }
 i += 2;
 _putchar('\n');
 
-return (0);
 }
 
