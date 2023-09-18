d5917042162b9db86906a608f4e2fb5c0121256c
fatma <fatmacivil@hotmail.com>
2023-09-18 07:22:41 -0700

    666

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,14 +9,13 @@
  */
 void puts2(char *str)
 {
-int i = 0;
-while (str[i] != '\0')
+int i;
+for (i = 0 ; str[i] != '\0' ; ++i)
 {
 _putchar(str[i]);
-i++;
-}
+
 if (i % 2 == 0)
-{
+
 _putchar(str[i]);
 }
 _putchar('\n');
