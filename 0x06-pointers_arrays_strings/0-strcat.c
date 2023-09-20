80e0be2e26835f3646a16defed5bf353eee74d61
fatma <fatmacivil@hotmail.com>
2023-09-20 08:23:57 -0700

    22

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,12 +12,12 @@
  */
 char *_strcat(char *dest, char *src)
 {
-int s1, s2;
-s1 = 0;
-while (dest[s1])
-s1++;
-for (s2 = 0 ; src[s2] ; s2++)
-dest[s1++] = src[s2];
+int s, s1;
+s = 0;
+while (dest[s])
+s++;
+for (s1 = 0 ; src[s1] ; s1++)
+dest[s++] = src[s1];
 return (dest);
 }
 
