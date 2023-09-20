74d037537ac4fe96569dbcaf2256a2f9d05a8e8d
fatma <fatmacivil@hotmail.com>
2023-09-20 09:51:30 -0700

    88

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -18,14 +18,10 @@ char *_strncat(char *dest, char *src, int n)
 int s, i;
 s = 0;
 while (dest[s])
-{
-s++
-}
+s++;
 for (i = 0 ; i < n && src[i] != '\0' ; i++)
-{
 dest[s + i] = src[i];
 dest[s + i] = '\0';
-}
 return (dest);
 }
 
