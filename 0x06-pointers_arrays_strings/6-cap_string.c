8c6ff188d799eca3768ef932e93ca9956a827c9e
fatma <fatmacivil@hotmail.com>
2023-09-20 10:14:23 -0700

    yy

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,64 +1,42 @@
 #include "main.h"
-#include <stdio.h>
-/**
- * is_lowercase - Checks if a character is a lowercase letter.
- * @ch: The character to check.
- *
- * Return: 1 if the character is a lowercase letter, 0 otherwise.
- */
-int  is_lowercase(char ch)
-{
-return (ch >= 'a' && ch <= 'z');
-}
-/**
- * is_delimiter - Checks if a character is a delimiter.
- * @ch: The character to check.
- *
- * Return: true if the character is a delimiter, false otherwise.
- */
-int is_delimiter(char ch)
-{
-char sep[] = " \t\n,;.!?\"(){}";
-int i;
-for (i = 0 ; i < 12 ; i++)
-{
-if (ch == sep[i])
-{
-return (1);
-}
-}
-return (0);
-}
+
 /**
  * cap_string - Capitalizes the first letter of each word in a string.
  * @str: The input string.
  *
  * Description:
  * This function takes an input string 'str' and capitalizes the first letter
- * ',', ';', '.', '!', '?', '"', '(', ')', '{', and '}'.
+ * of each word in the string where words are separated by specified separators:
+ * space, tabulation, newline, ',', ';', '.', '!', '?', '"', '(', ')', '{', and '}'.
  * It modifies 'str' in place.
  *
  * Return: A pointer to the modified string 'str'.
  */
+
 char *cap_string(char *str)
 {
-char *ptr = str;
-int new_word = 1;
-while (*str)
+int index = 0;
+while (str[index])
 {
-if (is_delimiter(*ch))
-new_word = 1;
-else if (is_lowercase(*ch) && new_word)
-{
-*srt -= 32;
-new_word = 0;
+while (!(str[index] >= 'a' && str[index] <= 'z'))
+index++;
+if (str[index - 1] == ' ' ||
+str[index - 1] == '\t' ||
+str[index - 1] == '\n' ||
+str[index - 1] == ',' ||
+str[index - 1] == ';' ||
+str[index - 1] == '.' ||
+str[index - 1] == '!' ||
+str[index - 1] == '?' ||
+str[index - 1] == '"' ||
+str[index - 1] == '(' ||
+str[index - 1] == ')' ||
+str[index - 1] == '{' ||
+str[index - 1] == '}' |
+index == 0)
+str[index] -= 32;
+index++;
 }
-else
-{
-new_word = 0;
-str++
-}
-return (ptr);
-}
+return (str);
 }
 
