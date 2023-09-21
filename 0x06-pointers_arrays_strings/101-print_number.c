2893599bb024cdf80d3b8e1482bda0aa851c2058
fatma <fatmacivil@hotmail.com>
2023-09-20 22:18:43 -0700

    cc

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -17,20 +17,17 @@
 
 void print_number(int n)
 {
+unsigned int n0;
+n0 = n;
 if (n < 0)
 {
 _putchar('-');
-n = -n;
+n0 = -n;
 }
-if (n == 0)
+if (n0 / 10 != 0)
 {
-_putchar('0');
-return;
+print_number(n0 / 10);
 }
-if (n / 10 != 0)
-{
-_putchar(n / 10);
-}
-_putchar('0' + n % 10);
+_putchar('0' + (n0 % 10));
 }
 
