9666275de446d44d12eaa77c4c910da1e562e477
fatma <fatmacivil@hotmail.com>
2023-09-28 09:42:49 -0700

    mo

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -15,23 +15,17 @@
 
 int wildcmp(char *s1, char *s2)
 {
-if (*s1 == '\0')
+if (*s1 == '\0' && *s2 == '\0')
 {
-if (*s2 == '\0' || (*s2 == '*' && wildcmp(s1, s2 + 1)))
 return (1);
-}
-if (*s2 == '\0')
-{
-return (*s1 == '\0');
 }
 if (*s2 == '*')
 {
 return (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)));
 }
-if (*s1 == *s2)
+if (*s1 == *s2 || *s2 == '?')
 {
 return (wildcmp(s1 + 1, s2 + 1));
 }
 return (0);
 }
-
