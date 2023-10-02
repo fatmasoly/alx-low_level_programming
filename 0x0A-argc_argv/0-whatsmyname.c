42ad16ec50a6d73ea2dd32a89883d23253fad24a
fatma <fatmacivil@hotmail.com>
2023-10-02 04:31:53 -0700

    3

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
-int main(int argc, char const *arcv[])
+/**
+ * main - Entry point of the program
+ * @argc: The number of command-line arguments (unused in this example)
+ * @argv: An array of strings containing the command-line arguments
+ *
+ * Description: This program prints the name of the program itself, which is
+ * stored in argv[0]. It takes argc as a parameter (unused) and uses argv to
+ * access the program name.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
 {
-(void)argc;
 printf("%s\n", argv[0]);
 return (0);
-}
 }
 
