a547598421d4fe7e5bc14f42bef3d018bb760166
fatma <fatmacivil@hotmail.com>
2023-10-10 01:28:53 -0700

    11s

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef NEW_H
+#define NEW_H
+/**
+ * struct dog - Represents a dog with a name, age, and owner.
+ *
+ * @name:  Pointer to the name of the dog.
+ * @age:   Age of the dog as a floating-point number.
+ * @owner: Pointer to the name of the owner.
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
