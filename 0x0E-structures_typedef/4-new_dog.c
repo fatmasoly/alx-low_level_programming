4ce14bb0aeb182222d7c3b4c254cb3d285de59b3
fatma <fatmacivil@hotmail.com>
2023-10-10 03:22:56 -0700

    ggg

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -26,6 +26,7 @@ return (NULL);
 new_dog->name = strdup(name);
 if (new_dog->name == NULL)
 {
+free(new_dog->name);
 free(new_dog);
 return (NULL);
 }
@@ -33,7 +34,7 @@ new_dog->age = age;
 new_dog->owner = strdup(owner);
 if (new_dog->owner == NULL)
 {
-free(new_dog->name);
+free(new_dog->owner);
 free(new_dog);
 return (NULL);
 }
