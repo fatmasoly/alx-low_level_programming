11944c84fc146e866db1cf6108e55b937dff1f09
fatma <fatmacivil@hotmail.com>
2023-10-11 03:27:43 -0700

    cc

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,12 +10,13 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-int i;
-for (i = 0 ; i < size ; i++)
-{
+int i = 0;
+if (array && size && cmp)
+while (i < size)
 if (cmp(array[i]))
 {
 return (i);
+i++;
 }
 }
 return (-1);
