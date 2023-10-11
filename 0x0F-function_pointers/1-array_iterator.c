6d605735c23958a0cf5fe1e3d127b57fc5805b3c
fatma <fatmacivil@hotmail.com>
2023-10-11 03:14:56 -0700

    jj

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -8,10 +8,9 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-size_t i;
-for (i = 0 ; i < size ; i++)
-{
+int *end = array + size - 1;
+if (array && size && action)
+while (array <= end)
 action(*array++);
-}
 }
 
