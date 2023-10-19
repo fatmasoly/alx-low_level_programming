d68df5af3d0d7c664e86fd7ae2f4295259621dce
fatma <fatmacivil@hotmail.com>
2023-10-18 23:18:17 -0700

    1task

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * list_len - Counts the number of elements in a linked list.
+ * @h: A pointer to the head of the list.
+ *
+ * Return: The number of elements in the list.
+ */
+size_t list_len(const list_t *h)
+{
+size_t n = 0;
+while (h != NULL)
+{
+n++;
+h = h->next;
+}
+return (n);
+}
+
