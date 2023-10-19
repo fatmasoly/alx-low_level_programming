3b4add53cc78c8e727e3d846ef2ddae91305c956
fatma <fatmacivil@hotmail.com>
2023-10-19 10:22:01 -0700

    tr

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -9,15 +9,13 @@
  */
 void free_list(list_t *head)
 {
-list_t *curr;
-list_t *next;
-curr = head;
+list_t *temp;
 while (head != NULL)
 {
-next = curr->next;
-free(curr->str);
-free(curr);
-curr = next;
+temp = head;
+head = head->next;
+free(temp->str);
+free(temp);
 }
 }
 
