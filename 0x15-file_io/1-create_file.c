8db89380fa4f2132a5d69485743b9e0c52fa6436
fatma <fatmacivil@hotmail.com>
2023-10-30 05:47:59 -0700

    2nd

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,17 +10,15 @@
 int create_file(const char *filename, char *text_content)
 {
 int fd;
-int rw;
-int letters;
+ssize_t rw;
 if (filename == NULL)
-return (-1);
-fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-if (fd == -1)
 return (-1);
 if (!text_content)
 text_content = "";
-for (letters = 0 ; text_content[letters] ; letters++)
-rw = write(fd, text_content, letters);
+fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+if (fd == -1)
+return (-1);
+rw = write(fd, text_content, strlen(text_content));
 if (rw == -1)
 return (-1);
 close(fd);
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,7 +6,7 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-
+#include <string.h>
 
 
 int _putchar(char c);
