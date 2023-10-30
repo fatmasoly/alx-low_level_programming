dd26e717c3fd1d5616f0af93daa0d77fbaec7acb
fatma <fatmacivil@hotmail.com>
2023-10-30 06:18:46 -0700

    dd

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,7 +16,7 @@ return (-1);
 fd = open(filename, O_WRONLY, O_APPEND);
 if (fd == -1)
 return (-1);
-if (text_content)
+if (strlen(text_content))
 {
 app = write(fd, text_content, strlen(text_content));
 if (app == -1)
