8365d73b39c2f5901d4f2a55fcb54397be3ca8b7
fatma <fatmacivil@hotmail.com>
2023-10-30 09:41:18 -0700

    hh

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,4 +1,20 @@
 #include "main.h"
+/**
+ * _strlen - Calculates the length of a string.
+ *
+ * @s: The string to calculate the length of.
+ *
+ * Return: The length of the string.
+ */
+size_t _strlen(const char *s)
+{
+size_t length = 0;
+while (s[length] != '\0')
+{
+length++;
+}
+return length;
+}
 
 /**
  * append_text_to_file - Appends text to the end of a file.
@@ -12,13 +28,13 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 int fd;
 int app;
-int len = strlen(text_content);
+int len = _strlen(text_content);
 if (filename == NULL)
 return (-1);
 fd = open(filename, O_WRONLY | O_APPEND);
 if (fd == -1)
 return (-1);
-if (!len)
+if (!text_content)
 {
 close(fd);
 return (1);
