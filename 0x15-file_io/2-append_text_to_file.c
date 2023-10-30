f808506777ad6e20afd28aed387a0d2951bf8a0d
fatma <fatmacivil@hotmail.com>
2023-10-30 06:31:27 -0700

    sd

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,6 +10,7 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 int fd;
+int letters;
 int app;
 if (filename == NULL)
 return (-1);
@@ -18,9 +19,9 @@ if (fd == -1)
 return (-1);
 if (text_content)
 {
-app = write(fd, text_content, strlen(text_content));
+for (letters = 0 ; text_content[letters] ; letters++)
+app = write(fd, text_content, letters);
 if (app == -1)
-close(fd);
 return (-1);
 }
 close(fd);
