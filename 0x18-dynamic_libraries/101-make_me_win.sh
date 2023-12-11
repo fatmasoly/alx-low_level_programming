df89bbe21587d1c6d84372238d0abe7d192abee5
fatma <fatmacivil@hotmail.com>
2023-12-11 04:55:07 -0800

    3rd

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-wget -P https://github.com/fatmasoly/alx-low_level_programming/blob/d27332e7cadd0d71061121500b3c482446578ff1/0x18-dynamic_libraries/
+wget -P https://github.com/fatmasoly/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD=/tmp/libgiga.so
