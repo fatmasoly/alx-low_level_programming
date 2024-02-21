0dd88d06b05292816d48a2c3aeae4ced6f625c60
fatma <fatmacivil@hotmail.com>
2024-02-21 03:25:16 -0800

    grid

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,4 +1,26 @@
 #!/usr/bin/python3
+"""This Module calculates the perimeter of an island"""
+
+
+def water_around(grid, i, j):
+    """Returns the number of water cells"""
+    water = 0
+    if i <= 0 or not grid[i - 1][j]:
+        water += 1
+    if j <= 0 or not grid[i][j - 1]:
+        water += 1
+    if j >= len(grid[i]) - 1 or not grid[i][j + 1]:
+        water += 1
+    if i >= len(grid) - 1 or not grid[i + 1][j]:
+        water += 1
+    return water
+
 
 def island_perimeter(grid):
-
+    """Returns the perimeter of the island in grid."""
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j]:
+                perimeter += water_around(grid, i, j)
+    return perimeter
